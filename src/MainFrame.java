fa0b4a28edbbad9a22011c6a6bdb809795f2f8ef
alo_wu <ilya.bela@yandex.ru>
2021-04-29 01:01:11 +0300

    add: mainframe

---
+++ src/MainFrame.java
@@ -0,0 +1,186 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+
+@SuppressWarnings("serial")
+public class MainFrame extends JFrame {
+
+    private static final String FRAME_TITLE = "Клиент мгновенных сообщений";
+
+    private static final int FRAME_MINIMUM_WIDTH = 500;
+    private static final int FRAME_MINIMUM_HEIGHT = 500;
+
+    private static final int FROM_FIELD_DEFAULT_COLUMNS = 10;
+    private static final int TO_FIELD_DEFAULT_COLUMNS = 20;
+    private static final int INCOMING_AREA_DEFAULT_ROWS = 10;
+    private static final int OUTGOING_AREA_DEFAULT_ROWS = 5;
+
+    private static final int SMALL_GAP = 5;
+    private static final int MEDIUM_GAP = 10;
+    private static final int LARGE_GAP = 15;
+    private static final int SERVER_PORT = 4567;
+
+    private final JTextField textFieldFrom;
+    private final JTextField textFieldTo;
+
+    private final JTextArea textAreaIncoming;
+    private final JTextArea textAreaOutgoing;
+    private String date;
+
+    public MainFrame() {
+
+        super(FRAME_TITLE);
+
+        setMinimumSize(new Dimension(FRAME_MINIMUM_WIDTH, FRAME_MINIMUM_HEIGHT));
+
+        final Toolkit kit = Toolkit.getDefaultToolkit();
+
+        setLocation((kit.getScreenSize().width - getWidth()) / 2,
+                (kit.getScreenSize().height - getHeight()) / 2);
+
+        textAreaIncoming = new JTextArea(INCOMING_AREA_DEFAULT_ROWS, 0);
+        textAreaIncoming.setEditable(false);
+
+        final JScrollPane scrollPaneIncoming = new JScrollPane(textAreaIncoming);
+
+        final JLabel labelFrom = new JLabel("От");
+        final JLabel labelTo = new JLabel("Получатель");
+
+        textFieldFrom = new JTextField(FROM_FIELD_DEFAULT_COLUMNS);
+        textFieldTo = new JTextField(TO_FIELD_DEFAULT_COLUMNS);
+        textAreaOutgoing = new JTextArea(OUTGOING_AREA_DEFAULT_ROWS, 0);
+
+        final JScrollPane scrollPaneOutgoing = new JScrollPane(textAreaOutgoing);
+
+        final JPanel messagePanel = new JPanel();
+        messagePanel.setBorder(BorderFactory.createTitledBorder("Сообщение"));
+
+        final JButton sendButton = new JButton("Отправить");
+
+
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                date=getDateTime();
+                sendMessage();}
+        });
+
+        final GroupLayout layout2 = new GroupLayout(messagePanel);
+        messagePanel.setLayout(layout2);
+        layout2.setHorizontalGroup(layout2.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout2.createParallelGroup(Alignment.TRAILING)
+                        .addGroup(layout2
+                                .createSequentialGroup()
+                                .addComponent(labelFrom)
+                                .addGap(SMALL_GAP)
+                                .addComponent(textFieldFrom)
+                                .addGap(LARGE_GAP)
+                                .addComponent(labelTo)
+                                .addGap(SMALL_GAP)
+                                .addComponent(textFieldTo))
+                        .addComponent(scrollPaneOutgoing)
+                        .addComponent(sendButton))
+                .addContainerGap());
+
+        layout2.setVerticalGroup(layout2.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout2
+                        .createParallelGroup(Alignment.BASELINE)
+                        .addComponent(labelFrom)
+                        .addComponent(textFieldFrom).addComponent(labelTo).addComponent(textFieldTo))
+                .addGap(MEDIUM_GAP)
+                .addComponent(scrollPaneOutgoing)
+                .addGap(MEDIUM_GAP)
+                .addComponent(sendButton)
+                .addContainerGap());
+
+        final GroupLayout layout1 = new GroupLayout(getContentPane());
+        setLayout(layout1);
+        layout1.setHorizontalGroup(layout1.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout1.createParallelGroup()
+                        .addComponent(scrollPaneIncoming)
+                        .addComponent(messagePanel))
+                .addContainerGap());
+
+        layout1.setVerticalGroup(layout1.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(scrollPaneIncoming)
+                .addGap(MEDIUM_GAP)
+                .addComponent(messagePanel)
+                .addContainerGap());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+                    while (!Thread.interrupted()) {
+                        final Socket socket = serverSocket.accept();
+                        final DataInputStream in = new DataInputStream(socket.getInputStream());
+                        final String senderName = in.readUTF();
+                        final String message = in.readUTF();
+                        final String Dat = in.readUTF();
+                        socket.close();
+
+                        final String address =((InetSocketAddress) socket
+                                .getRemoteSocketAddress()).getAddress().getHostAddress();
+                        textAreaIncoming.append(Dat+"  "+senderName + " (" + address + "): " + message + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    JOptionPane.showMessageDialog(MainFrame.this, "Ошибка в работе сервера",
+                            "Ошибка", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }).start();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                final MainFrame frame = new MainFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+    private String getDateTime() {
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+        Date date = new Date();
+
+        return dateFormat.format(date);
+
+    }
+}
